9831d82e9253b44db09adf86789b1fd5549ac46c
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-20 22:38:24 +0530

    (Shubham)Add: Project folder structure created

---
+++ StacksAndQueuesProblems/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace StacksAndQueuesProblems;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Stacks And Queues Operations");
+    }
+}
